bacb7ad22662c002216b5d1c71812538b72e57de
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-12-15 18:39:25 +0800

    SECOND COMMIT

--- THREAD.py
+++ THREAD.py
@@ -1,5 +1,57 @@
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
+import threading
+from random import randint
+from time import sleep
+
+PRODUCTS = (
+    ":balloon:",
+    ":cookie:",
+    ":crystal_ball:",
+    ":diving_mask:",
+    ":flashlight:",
+    ":gem:",
+    ":gift:",
+    ":kite:",
+    ":party_popper:",
+    ":postal_horn:",
+    ":ribbon:",
+    ":rocket:",
+    ":teddy_bear:",
+    ":thread:",
+    ":yo-yo:",
+)
+class Worker(threading.Thread):
+    def __init__(self, speed, buffer):
+        super().__init__(daemon=True)
+        self.speed = speed
+        self.buffer = buffer
+        self.product = None
+        self.working = False
+        self.progress = 0
+
+
+class Worker(threading.Thread):
+
+    @property
+    def state(self):
+        if self.working:
+            return f"{self.product} ({self.progress}%)"
+        return ":zzz: Idle"
+
+    def simulate_idle(self):
+        self.product = None
+        self.working = False
+        self.progress = 0
+        sleep(randint(1, 3))
+
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
 
 QUEUE_TYPES = {
     "fifo": Queue,
@@ -24,3 +76,4 @@ if __name__ == "__main__":
         main(parse_args())
     except KeyboardInterrupt:
         pass
+
