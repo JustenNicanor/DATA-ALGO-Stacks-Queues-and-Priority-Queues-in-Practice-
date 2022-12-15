128b89835533fdbb245e1dba5716d48394453b7a
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2022-12-15 20:06:26 +0800

    THIRD COMMIT

--- THREAD.py
+++ THREAD.py
@@ -4,6 +4,20 @@ import threading
 from random import randint
 from time import sleep
 
+from itertools import zip_longest
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.live import Live
+from rich.panel import Panel
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
+
 PRODUCTS = (
     ":balloon:",
     ":cookie:",
@@ -21,6 +35,54 @@ PRODUCTS = (
     ":thread:",
     ":yo-yo:",
 )
+
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
+
+    def animate(self):
+        with Live(
+            self.render(), screen=True, refresh_per_second=10
+        ) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(
+            padding + worker.state, align="left", vertical="middle"
+        )
+        return Panel(align, height=5, title=title)
+    
 class Worker(threading.Thread):
     def __init__(self, speed, buffer):
         super().__init__(daemon=True)
@@ -29,9 +91,6 @@ class Worker(threading.Thread):
         self.product = None
         self.working = False
         self.progress = 0
-
-
-class Worker(threading.Thread):
 
     @property
     def state(self):
@@ -53,11 +112,6 @@ class Worker(threading.Thread):
             sleep(delay / 100)
             self.progress += 1
 
-QUEUE_TYPES = {
-    "fifo": Queue,
-    "lifo": LifoQueue,
-    "heap": PriorityQueue
-}
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
